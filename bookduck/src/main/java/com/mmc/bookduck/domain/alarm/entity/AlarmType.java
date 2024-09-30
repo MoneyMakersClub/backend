01691036d439ad029248eb77baa09f9ad2452b75
seohyun-lee <applemint2003@gmail.com>
2024-10-01 00:53:11 +0900

    feat: AlarmType 이넘 종류 변경, 메시지 형식 필드 삭제

--- bookduck/src/main/java/com/mmc/bookduck/domain/alarm/entity/AlarmType.java
+++ bookduck/src/main/java/com/mmc/bookduck/domain/alarm/entity/AlarmType.java
@@ -1,17 +1,12 @@
 package com.mmc.bookduck.domain.alarm.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public enum AlarmType {
-    FRIEND_REQUEST("{0}님이 친구 요청을 보냈어요."),
-    FRIEND_APPROVED("{0}님과 친구가 되었어요."),
-    REVIEW_HEART_ADDED("{0}님이 내 기록에 좋아요를 눌렀어요."),
-    ANNOUNCEMENT(""),
-    EVENT(""),
-    ;
-
-    private final String messagePattern;
+    FRIEND_REQUEST,
+    FRIEND_APPROVED,
+    REVIEW_HEART_ADDED,
+    LEVEL_UP,
+    ANNOUNCEMENT,
+    EVENT,
+    SKIN_UNLOCKED,
+    BADGE_UNLOCKED
 }
