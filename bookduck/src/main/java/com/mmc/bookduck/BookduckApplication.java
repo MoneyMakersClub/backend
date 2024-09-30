a299277fbe137c21e73ece414bb44b249e9bbe49
seohyun-lee <applemint2003@gmail.com>
2024-09-30 10:10:37 +0900

    feat: 스프링 프로젝트 생성

---
+++ bookduck/src/main/java/com/mmc/bookduck/BookduckApplication.java
@@ -0,0 +1,13 @@
+package com.mmc.bookduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookduckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookduckApplication.class, args);
+	}
+
+}
---
+++ bookduck/src/test/java/com/mmc/bookduck/BookduckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmc.bookduck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookduckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
