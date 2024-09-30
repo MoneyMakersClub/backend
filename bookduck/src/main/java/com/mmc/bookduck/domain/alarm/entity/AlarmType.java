5e9d377534d731b13d40f448bfd7f38ca3015ecb
seohyun-lee <applemint2003@gmail.com>
2024-10-01 00:55:53 +0900

    feat: AlarmType 이넘 값 변경

--- bookduck/src/main/java/com/mmc/bookduck/domain/alarm/entity/AlarmType.java
+++ bookduck/src/main/java/com/mmc/bookduck/domain/alarm/entity/AlarmType.java
@@ -1,12 +1,13 @@
 package com.mmc.bookduck.domain.alarm.entity;
 
 public enum AlarmType {
+    DEFAULT,
+    ANNOUNCEMENT,
     FRIEND_REQUEST,
     FRIEND_APPROVED,
     REVIEW_HEART_ADDED,
     LEVEL_UP,
-    ANNOUNCEMENT,
-    EVENT,
     SKIN_UNLOCKED,
-    BADGE_UNLOCKED
+    BADGE_UNLOCKED,
+    PUSH
 }
