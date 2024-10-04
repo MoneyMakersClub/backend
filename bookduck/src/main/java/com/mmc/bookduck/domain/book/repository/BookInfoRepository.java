2b02742a4ed7e2087c7c74fc284e226ff6733f21
hyejin2234 <hhj020304@naver.com>
2024-10-04 18:29:02 +0900

    feat: 도서 추가 정보 dto 생성

--- bookduck/src/main/java/com/mmc/bookduck/domain/book/repository/BookInfoRepository.java
+++ bookduck/src/main/java/com/mmc/bookduck/domain/book/repository/BookInfoRepository.java
@@ -4,4 +4,5 @@ import com.mmc.bookduck.domain.book.entity.BookInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookInfoRepository extends JpaRepository<BookInfo, Long> {
+    BookInfo findByProviderId(String providerId);
 }
