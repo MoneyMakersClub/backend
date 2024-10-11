cb5bda2569c6e287584c74362de9b21d8057dccd
seohyun-lee <applemint2003@gmail.com>
2024-10-11 13:25:46 +0900

    feat: HomeBlockRepository 추가

---
+++ bookduck/src/main/java/com/mmc/bookduck/domain/user/repository/HomeBlockRepository.java
@@ -0,0 +1,7 @@
+package com.mmc.bookduck.domain.user.repository;
+
+import com.mmc.bookduck.domain.user.entity.HomeBlock;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HomeBlockRepository extends JpaRepository<HomeBlock, Long> {
+}
