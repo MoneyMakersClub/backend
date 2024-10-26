d0559a41f5d09fa6dd7d5ddb447cc81b2f8d9e64
hyejin2234 <hhj020304@naver.com>
2024-10-26 15:15:28 +0900

    feat: hashMap 전역으로 변경

--- bookduck/src/main/java/com/mmc/bookduck/domain/book/service/GenreService.java
+++ bookduck/src/main/java/com/mmc/bookduck/domain/book/service/GenreService.java
@@ -6,7 +6,6 @@ import com.mmc.bookduck.domain.book.repository.GenreRepository;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import com.mmc.bookduck.global.exception.CustomException;
 import com.mmc.bookduck.global.exception.ErrorCode;
@@ -20,6 +19,56 @@ import org.springframework.transaction.annotation.Transactional;
 public class GenreService {
 
     private final GenreRepository genreRepository;
+
+    // category랑 GenreName 매핑정보
+    private static final Map<String, GenreName> genreMap = new HashMap<>() {{
+        put("JUVENILE", GenreName.YOUTH);
+        put("YOUNG ADULT", GenreName.YOUTH);
+        put("POETRY", GenreName.LITERARY);
+        put("BUSINESS", GenreName.BUSINESS);
+        put("SELF-HELP", GenreName.SELF_HELP);
+        put("TRAVEL", GenreName.TRAVEL);
+        put("HISTORY", GenreName.HISTORY);
+        put("COMPUTER", GenreName.COMPUTER);
+        put("LANGUAGE", GenreName.LANGUAGE);
+        put("SOCIAL SCIENCE", GenreName.SOCIETY);
+        put("POLITICAL", GenreName.SOCIETY);
+        put("LAW", GenreName.SOCIETY);
+        put("MATH", GenreName.SCIENCE);
+        put("NATURE", GenreName.SCIENCE);
+        put("SCIENCE", GenreName.SCIENCE);
+        put("BIBLE", GenreName.RELIGION);
+        put("RELIGION", GenreName.RELIGION);
+        put("COMICS", GenreName.COMICS);
+        put("COOKING", GenreName.HOME_COOKING);
+        put("FAMILY", GenreName.HOME_COOKING);
+        put("HOUSE", GenreName.HOME_COOKING);
+        put("HOME", GenreName.HOME_COOKING);
+        put("BODY", GenreName.HEALTH);
+        put("MIND", GenreName.HEALTH);
+        put("SPORT", GenreName.HEALTH);
+        put("HEALTH", GenreName.HEALTH);
+        put("ARCHITECTURE", GenreName.ARCHITECTURE);
+        put("REFERENCE", GenreName.REFERENCE);
+        put("STUDY", GenreName.REFERENCE);
+        put("HOBBIES", GenreName.HOBBY);
+        put("GAME", GenreName.HOBBY);
+        put("GARDEN", GenreName.HOBBY);
+        put("PET", GenreName.HOBBY);
+        put("ART", GenreName.ART);
+        put("MUSIC", GenreName.ART);
+        put("DESIGN", GenreName.ART);
+        put("PHOTO", GenreName.ART);
+        put("DRAMA", GenreName.ART);
+        put("MEDICAL", GenreName.TECHNOLOGY);
+        put("TECHNOLOGY", GenreName.TECHNOLOGY);
+        put("TRANSPORTATION", GenreName.TECHNOLOGY);
+        put("CRITICISM", GenreName.HUMANITIES);
+        put("PHILOSOPHY", GenreName.HUMANITIES);
+        put("PSYCHOLOGY", GenreName.HUMANITIES);
+        put("EDUCATION", GenreName.HUMANITIES);
+        put("FICTION", GenreName.FICTION);
+    }};
 
 
     public Genre matchGenre(List<String> apiCategory) {
@@ -63,57 +112,6 @@ public class GenreService {
             return GenreName.LITERARY;
         }
 
-        Map<String, GenreName> genreMap = new HashMap<>();
-        // JUVENILE & YOUNG ADULT을 FICTION보다 우선 체크 (JUVENILE FICTION이 YOUTH로 매핑)
-        genreMap.put("JUVENILE", GenreName.YOUTH);
-        genreMap.put("YOUNG ADULT", GenreName.YOUTH);
-        genreMap.put("POETRY", GenreName.LITERARY);
-        genreMap.put("BUSINESS", GenreName.BUSINESS);
-        genreMap.put("SELF-HELP", GenreName.SELF_HELP);
-        genreMap.put("TRAVEL", GenreName.TRAVEL);
-        genreMap.put("HISTORY", GenreName.HISTORY);
-        genreMap.put("COMPUTER", GenreName.COMPUTER);
-        // LANGUAGE를 ART보다 먼저 체크(LANGUAGE ARTS가 LANGUAGE로 매핑)
-        genreMap.put("LANGUAGE", GenreName.LANGUAGE);
-        // SOCIAL SCIENCE & POLITICAL SCIENCE를 SCIENCE보다 먼저 체크
-        genreMap.put("SOCIAL SCIENCE", GenreName.SOCIETY);
-        genreMap.put("POLITICAL", GenreName.SOCIETY);
-        genreMap.put("LAW", GenreName.SOCIETY);
-        genreMap.put("MATH", GenreName.SCIENCE);
-        genreMap.put("NATURE", GenreName.SCIENCE);
-        genreMap.put("SCIENCE", GenreName.SCIENCE);
-        genreMap.put("BIBLE", GenreName.RELIGION);
-        genreMap.put("RELIGION", GenreName.RELIGION);
-        genreMap.put("COMICS", GenreName.COMICS);
-        genreMap.put("COOKING", GenreName.HOME_COOKING);
-        genreMap.put("FAMILY", GenreName.HOME_COOKING);
-        genreMap.put("HOUSE", GenreName.HOME_COOKING);
-        genreMap.put("HOME", GenreName.HOME_COOKING);
-        genreMap.put("BODY", GenreName.HEALTH);
-        genreMap.put("MIND", GenreName.HEALTH);
-        genreMap.put("SPORT", GenreName.HEALTH);
-        genreMap.put("HEALTH", GenreName.HEALTH);
-        genreMap.put("ARCHITECTURE", GenreName.ARCHITECTURE);
-        genreMap.put("REFERENCE", GenreName.REFERENCE);
-        genreMap.put("STUDY", GenreName.REFERENCE);
-        genreMap.put("HOBBIES", GenreName.HOBBY);
-        genreMap.put("GAME", GenreName.HOBBY);
-        genreMap.put("GARDEN", GenreName.HOBBY);
-        genreMap.put("PET", GenreName.HOBBY);
-        genreMap.put("ART", GenreName.ART);
-        genreMap.put("MUSIC", GenreName.ART);
-        genreMap.put("DESIGN", GenreName.ART);
-        genreMap.put("PHOTO", GenreName.ART);
-        genreMap.put("DRAMA", GenreName.ART);
-        genreMap.put("MEDICAL", GenreName.TECHNOLOGY);
-        genreMap.put("TECHNOLOGY", GenreName.TECHNOLOGY);
-        genreMap.put("TRANSPORTATION", GenreName.TECHNOLOGY);
-        genreMap.put("CRITICISM", GenreName.HUMANITIES);
-        genreMap.put("PHILOSOPHY", GenreName.HUMANITIES);
-        genreMap.put("PSYCHOLOGY", GenreName.HUMANITIES);
-        genreMap.put("EDUCATION", GenreName.HUMANITIES);
-        genreMap.put("FICTION", GenreName.FICTION);
-
         String upperC = category.toUpperCase();
         for (Map.Entry<String, GenreName> entry : genreMap.entrySet()) {
             if (upperC.contains(entry.getKey())) {
@@ -151,6 +149,7 @@ public class GenreService {
         };
     }
 
+    @Transactional(readOnly = true)
     public Genre findGenreById(Long genreId) {
         return genreRepository.findById(genreId)
                 .orElseThrow(()-> new CustomException(ErrorCode.GENRE_NOT_FOUND));
