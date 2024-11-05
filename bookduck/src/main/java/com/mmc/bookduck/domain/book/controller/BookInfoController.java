596f184c980405897195ac4f7d823a5a4497e187
hyejin2234 <hhj020304@naver.com>
2024-11-05 22:15:37 +0900

    feat: @Tag, @Operation 추가

--- bookduck/src/main/java/com/mmc/bookduck/domain/book/controller/BookInfoController.java
+++ bookduck/src/main/java/com/mmc/bookduck/domain/book/controller/BookInfoController.java
@@ -6,10 +6,13 @@ import com.mmc.bookduck.domain.book.dto.response.BookInfoBasicResponseDto;
 import com.mmc.bookduck.domain.book.dto.response.BookListResponseDto;
 import com.mmc.bookduck.domain.book.dto.response.BookUnitResponseDto;
 import com.mmc.bookduck.domain.book.service.BookInfoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "BookInfo", description = "BookInfo 관련 API입니다.")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/bookinfo")
@@ -17,7 +20,8 @@ public class BookInfoController {
 
     private final BookInfoService bookInfoService;
 
-    // 도서목록 GET(API)
+
+    @Operation(summary = "API 도서 목록 검색", description = "구글 API에서 특정 키워드에 해당하는 도서 목록을 검색합니다.")
     @GetMapping("/search")
     public ResponseEntity<BookListResponseDto<BookUnitResponseDto>> searchBookList(@RequestParam(name = "keyword") final String keyword,
                                                                                    @RequestParam final Long page,
@@ -26,26 +30,30 @@ public class BookInfoController {
         return ResponseEntity.ok(bookInfoService.searchBookList(keyword, page, size));
     }
 
-    // API 도서 상세 정보 GET - 기본 정보
+
+    @Operation(summary = "API 도서 상세-기본 정보 조회", description = "구글 API 도서의 기본 정보를 상세 조회합니다.(책 기본정보 + 현재 사용자의 별점&한줄평)")
     @GetMapping("/external/{providerId}")
     public ResponseEntity<BookInfoBasicResponseDto> getOneBookBasic(@PathVariable(name = "providerId") final String providerId){
         return ResponseEntity.ok(bookInfoService.getOneBookBasic(providerId));
     }
 
-    // API 도서 상세 조회 - 추가 정보
+
+    @Operation(summary = "API 도서 상세-추가 정보 조회", description = "구글 API 도서의 기본 정보를 상세 조회합니다.(현재 책에 대한 다른 사용자들의 별점&한줄평 목록 3개)")
     @GetMapping("/external/{providerId}/additional")
     public ResponseEntity<BookInfoAdditionalResponseDto> getApiBookAdditional(@PathVariable(name = "providerId") String providerId){
         return ResponseEntity.ok(bookInfoService.getApiBookAdditional(providerId));
     }
 
-    // 도서목록 GET(CUSTOM)
+
+    @Operation(summary = "사용자가 직접 추가한 도서 목록 검색", description = "현재 사용자가 직접 추가한 도서 중에서 특정 키워드에 해당하는 도서 목록을 검색합니다.")
     @GetMapping("/search/custom")
     public ResponseEntity<BookListResponseDto<CustomBookUnitResponseDto>> searchCustomBookList(@RequestParam(name = "keyword") final String keyword, @RequestParam final Long page,
                                                                                                @RequestParam final Long size){
         return ResponseEntity.ok(bookInfoService.searchCustomBookList(keyword, page, size));
     }
 
-    // 커스텀 도서 상세 조회 - 기본 정보 + 사용자의 한줄평,별점
+
+    @Operation(summary = "사용자가 직접 추가한 도서 상세-기본 정보 조회", description = "사용자가 직접 추가한 도서의 기본 정보를 상세 조회합니다.(책 기본정보 + 현재 사용자의 별점&한줄평)")
     @GetMapping("/custom/{bookinfoId}/additional")
     public ResponseEntity<BookInfoBasicResponseDto> getCustomBookBasic(@PathVariable(name = "bookinfoId") final Long bookInfoId){
         return ResponseEntity.ok(bookInfoService.getCustomBookBasic(bookInfoId));
