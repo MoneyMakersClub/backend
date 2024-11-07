c4ec4b84d031b9afe698ba27f7c293eb6e041762
judith <8w8u8@naver.com>
2024-11-07 13:23:11 +0900

    fix: 친구/친구요청 응답dto 구성 수정

--- bookduck/src/main/java/com/mmc/bookduck/domain/friend/dto/common/FriendRequestUnitDto.java
+++ bookduck/src/main/java/com/mmc/bookduck/domain/friend/dto/common/FriendRequestUnitDto.java
@@ -1,18 +1,20 @@
 package com.mmc.bookduck.domain.friend.dto.common;
 
 import com.mmc.bookduck.domain.friend.entity.FriendRequest;
-import com.mmc.bookduck.domain.friend.entity.FriendRequestStatus;
+import com.mmc.bookduck.domain.item.dto.common.UserItemEquippedDto;
 
 public record FriendRequestUnitDto(
         Long requestId,
+        Long userId,
         String receiverNickname,
-        FriendRequestStatus friendRequestStatus
+        UserItemEquippedDto userItemEquipped
 ) {
-    public static FriendRequestUnitDto from(FriendRequest friendRequest) {
+    public static FriendRequestUnitDto from(FriendRequest friendRequest, UserItemEquippedDto userItemEquipped) {
         return new FriendRequestUnitDto(
                 friendRequest.getRequestId(),
+                friendRequest.getReceiver().getUserId(),
                 friendRequest.getReceiver().getNickname(),
-                friendRequest.getFriendRequestStatus()
+                userItemEquipped
         );
     }
 }
