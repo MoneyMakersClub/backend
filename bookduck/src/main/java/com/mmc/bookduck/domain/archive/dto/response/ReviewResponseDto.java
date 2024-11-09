d1537faf66664fbd0b901620b40111c3faff0f69
judith <8w8u8@naver.com>
2024-11-09 12:24:59 +0900

    fix: 기록 응답 DTO에 생성, 변경시간 추가

--- bookduck/src/main/java/com/mmc/bookduck/domain/archive/dto/response/ReviewResponseDto.java
+++ bookduck/src/main/java/com/mmc/bookduck/domain/archive/dto/response/ReviewResponseDto.java
@@ -3,20 +3,26 @@ package com.mmc.bookduck.domain.archive.dto.response;
 import com.mmc.bookduck.domain.archive.entity.Review;
 import com.mmc.bookduck.domain.common.Visibility;
 
+import java.time.LocalDateTime;
+
 public record ReviewResponseDto(
         Long ReviewId,
         String reviewTitle,
         String reviewContent,
+        String color,
         Visibility visibility,
-        String color
+        LocalDateTime createdTime,
+        LocalDateTime modifiedTime
 ) {
     public static ReviewResponseDto from(Review review) {
         return new ReviewResponseDto(
                 review.getReviewId(),
                 review.getReviewTitle(),
                 review.getReviewContent(),
+                review.getColor(),
                 review.getVisibility(),
-                review.getColor()
+                review.getCreatedTime(),
+                review.getModifiedTime()
         );
     }
 }
