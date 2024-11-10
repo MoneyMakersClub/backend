1e13adb6bc5c720cf1cdac6878bdcc196009e006
judith <8w8u8@naver.com>
2024-11-10 22:32:07 +0900

    Hotfix: 인증 관련 코드 변경

--- bookduck/src/main/java/com/mmc/bookduck/global/google/GoogleCloudUploadService.java
+++ bookduck/src/main/java/com/mmc/bookduck/global/google/GoogleCloudUploadService.java
@@ -1,10 +1,15 @@
 package com.mmc.bookduck.global.google;
 
+import autovalue.shaded.com.google.common.collect.Lists;
+import com.google.auth.oauth2.GoogleCredentials;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import com.google.cloud.storage.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -12,13 +17,33 @@ import com.mmc.bookduck.global.exception.CustomException;
 import com.mmc.bookduck.global.exception.ErrorCode;
 
 @Component
-@RequiredArgsConstructor
 public class GoogleCloudUploadService {
 
     @Value("${cloud.gcp.storage.bucket.name}")
     private String bucketName;
 
-    private static Storage storage = StorageOptions.getDefaultInstance().getService();
+    @Value("${cloud.gcp.storage.credentials.location}")
+    private String credentialsFilePath;
+
+    @Value("${cloud.gcp.storage.project-id}")
+    private String projectId;
+
+    private Storage storage;
+
+    @PostConstruct
+    public void init() {
+        try {
+            GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsFilePath))
+                    .createScoped("https://www.googleapis.com/auth/cloud-platform");
+            this.storage = StorageOptions.newBuilder()
+                    .setProjectId(projectId)
+                    .setCredentials(credentials)
+                    .build()
+                    .getService();
+        } catch (IOException e) {
+            throw new RuntimeException("GCS 초기화 실패", e);
+        }
+    }
 
     public String upload(MultipartFile file) {
         try {
