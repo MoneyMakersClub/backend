524153b40f2790196c67a9adf1346db23aee918e
judith <8w8u8@naver.com>
2024-11-11 13:07:41 +0900

    Hotfix: 파일 경로 환경변수 방식으로 변경

--- bookduck/src/main/java/com/mmc/bookduck/global/config/GoogleCloudConfig.java
+++ bookduck/src/main/java/com/mmc/bookduck/global/config/GoogleCloudConfig.java
@@ -6,26 +6,22 @@ import com.google.cloud.storage.StorageOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class GoogleCloudConfig {
 
-    @Value("mmc-bookduck-ocr.json")
-    private ClassPathResource gcpServiceAccountKey;
+    @Value("${cloud.gcp.storage.credentials.location}")
+    private String gcpAccountKeyPath;
 
     @Value("${cloud.gcp.storage.project-id}")
     private String projectId;
 
     @Bean
     public Storage storage() throws IOException {
-        System.out.println(readResourceAsString(gcpServiceAccountKey));
-        GoogleCredentials credentials = GoogleCredentials.fromStream(gcpServiceAccountKey.getInputStream());
+        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(gcpAccountKeyPath));
         return StorageOptions.newBuilder()
                 .setProjectId(projectId)
                 .setCredentials(credentials)
@@ -33,10 +29,4 @@ public class GoogleCloudConfig {
                 .getService();
     }
 
-    // 디버깅
-    private String readResourceAsString(ClassPathResource resource) throws IOException {
-        try (InputStream inputStream = resource.getInputStream()) {
-            return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
-        }
-    }
 }
