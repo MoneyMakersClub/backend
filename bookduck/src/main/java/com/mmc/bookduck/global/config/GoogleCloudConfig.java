e113e4393258390dc70f94b7197847e303d3a13f
judith <8w8u8@naver.com>
2024-11-11 13:45:44 +0900

    Hotfix: 환경변수 주입 디버깅 추가

--- bookduck/src/main/java/com/mmc/bookduck/global/config/GoogleCloudConfig.java
+++ bookduck/src/main/java/com/mmc/bookduck/global/config/GoogleCloudConfig.java
@@ -3,6 +3,7 @@ package com.mmc.bookduck.global.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +19,11 @@ public class GoogleCloudConfig {
 
     @Value("${cloud.gcp.storage.project-id}")
     private String projectId;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("gcpAccountKeyPath: " + gcpAccountKeyPath);
+    }
 
     @Bean
     public Storage storage() throws IOException {
