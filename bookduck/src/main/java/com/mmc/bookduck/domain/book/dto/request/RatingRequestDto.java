97385949ae7cf7583fdb807e3f84cab1de430705
hyejin2234 <hhj020304@naver.com>
2024-11-12 15:16:52 +0900

    fix: 별점 최소,최대값 유효성검사 추가

--- bookduck/src/main/java/com/mmc/bookduck/domain/book/dto/request/RatingRequestDto.java
+++ bookduck/src/main/java/com/mmc/bookduck/domain/book/dto/request/RatingRequestDto.java
@@ -1,8 +1,10 @@
 package com.mmc.bookduck.domain.book.dto.request;
 
-import com.mmc.bookduck.domain.book.entity.UserBook;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 
-public record RatingRequestDto(@NotNull double rating){
+public record RatingRequestDto(@NotNull
+                               @Min(value = 1, message = "최소 별점은 1점입니다.")
+                               @Max(value = 5, message = "최대 별점은 5점입니다.") double rating){
 }
