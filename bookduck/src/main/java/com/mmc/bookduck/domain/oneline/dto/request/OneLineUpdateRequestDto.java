75f5b70187daf065940f2c82953d3cf10442212d
judith <8w8u8@naver.com>
2024-11-14 15:03:41 +0900

    fix: 오타 수정

--- bookduck/src/main/java/com/mmc/bookduck/domain/oneline/dto/request/OneLineUpdateRequestDto.java
+++ bookduck/src/main/java/com/mmc/bookduck/domain/oneline/dto/request/OneLineUpdateRequestDto.java
@@ -3,6 +3,6 @@ package com.mmc.bookduck.domain.oneline.dto.request;
 import jakarta.validation.constraints.NotNull;
 
 public record OneLineUpdateRequestDto(
-        @NotNull String OneLineContent
+        @NotNull String oneLineContent
 ) {
 }
