6176ae438455c5fe14ffc770c98833e3daf7f3cc
judith <8w8u8@naver.com>
2024-11-17 05:57:56 +0900

    fix: 글자수 제한 추가

--- bookduck/src/main/java/com/mmc/bookduck/domain/archive/entity/Review.java
+++ bookduck/src/main/java/com/mmc/bookduck/domain/archive/entity/Review.java
@@ -6,6 +6,7 @@ import com.mmc.bookduck.domain.user.entity.User;
 import com.mmc.bookduck.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
@@ -19,10 +20,11 @@ public class Review extends BaseTimeEntity {
     @Column(updatable = false)
     private Long reviewId;
 
-    @NotNull
+    @Size(max = 25, message = "리뷰 제목은 최대 25자까지 입력할 수 있습니다.")
     private String reviewTitle;
 
     @NotNull
+    @Size(max = 1000, message = "리뷰 내용은 최대 1000자까지 입력할 수 있습니다.")
     private String reviewContent;
 
     @Enumerated(EnumType.STRING)
