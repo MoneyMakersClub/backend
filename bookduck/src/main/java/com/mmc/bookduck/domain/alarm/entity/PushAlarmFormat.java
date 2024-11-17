afcc2268c75d4421a6eea322eab963441dc04d70
seohyun-lee <applemint2003@gmail.com>
2024-11-17 23:21:55 +0900

    feat: PushAlarmFormat 수정

--- bookduck/src/main/java/com/mmc/bookduck/domain/alarm/entity/PushAlarmFormat.java
+++ bookduck/src/main/java/com/mmc/bookduck/domain/alarm/entity/PushAlarmFormat.java
@@ -8,7 +8,9 @@ import lombok.Getter;
 public enum PushAlarmFormat {
     FRIEND_REQUEST("{0}님으로부터 친구요청이 도착했어요!", "북덕에서 확인하세요."),
     FRIEND_APPROVED("{0}님이 친구요청을 수락했어요.", "북덕에서 확인하세요."),
-
+    ONELINELIKE_ADDED("{0}의 한줄평에 좋아요가 눌렸어요.", "북덕에서 확인하세요."),
+    LEVEL_UP("야호! 오리가 Lv.{0}로 성장했어요.", "북덕에서 확인하세요."),
+    BADGE_UNLOCKED("축하합니다! \uD83C\uDF89 새로운 {0} 뱃지를 획득했어요.", "북덕에서 확인하세요."),
     ;
 
     private final String title;
