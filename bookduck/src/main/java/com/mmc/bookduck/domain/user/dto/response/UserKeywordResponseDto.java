3c2da627550ce1569be43bbffb825c879c0439e9
judith <8w8u8@naver.com>
2024-11-18 15:35:21 +0900

    fix: @JsonValue 추가

--- bookduck/src/main/java/com/mmc/bookduck/domain/user/dto/response/UserKeywordResponseDto.java
+++ bookduck/src/main/java/com/mmc/bookduck/domain/user/dto/response/UserKeywordResponseDto.java
@@ -1,10 +1,17 @@
 package com.mmc.bookduck.domain.user.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 import java.util.List;
 
 public record UserKeywordResponseDto(
         List<String> keywords
 ) {
+    @JsonValue
+    public List<String> toJson() {
+        return keywords;
+    }
+
     public static UserKeywordResponseDto from(List<String> keywords, int limit) {
         return new UserKeywordResponseDto(
                 keywords.stream()
