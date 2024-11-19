b4bdac35f63d233b962d6519227c26adde87a200
seohyun-lee <applemint2003@gmail.com>
2024-11-19 19:48:11 +0900

    refactor: 쓰이지 않는 임포트 제거

--- bookduck/src/main/java/com/mmc/bookduck/domain/book/dto/common/BookUnitParseDto.java
+++ bookduck/src/main/java/com/mmc/bookduck/domain/book/dto/common/BookUnitParseDto.java
@@ -1,7 +1,6 @@
 package com.mmc.bookduck.domain.book.dto.common;
 
 import jakarta.validation.constraints.NotNull;
-import java.util.List;
 
 public record BookUnitParseDto(@NotNull String title,
                                String author,
