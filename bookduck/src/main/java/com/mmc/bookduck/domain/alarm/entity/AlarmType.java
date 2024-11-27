8e43cf693d8bae96b6bd4d89c9f783d98faf8007
seohyun-lee <applemint2003@gmail.com>
2024-11-28 02:42:38 +0900

    feat: SSE 알림 응답 객체 수정, 아이템 알림 AlarmType에 추가, User에서 isItemUnlockedChecked 삭제 등
    아이템 알림 최초 1회만 뜨도록 설정

--- bookduck/src/main/java/com/mmc/bookduck/domain/alarm/entity/AlarmType.java
+++ bookduck/src/main/java/com/mmc/bookduck/domain/alarm/entity/AlarmType.java
@@ -15,6 +15,7 @@ public enum AlarmType {
     ONELINELIKE_ADDED("{0}의 한줄평에 좋아요가 눌렸어요.", true),
     LEVEL_UP("야호! 오리가 Lv.{0}로 성장했어요.", false),
     BADGE_UNLOCKED("축하합니다! \uD83C\uDF89 새로운 {0} 뱃지를 획득했어요.", false),
+    ITEM_UNLOCKED("새로운 아이템을 획득했어요. 얼른 착용해봐요!", false),
     PUSH("",true), // 기타 푸시알림
     ;
 
--- bookduck/src/main/java/com/mmc/bookduck/domain/badge/entity/BadgeType.java
+++ bookduck/src/main/java/com/mmc/bookduck/domain/badge/entity/BadgeType.java
@@ -6,12 +6,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum BadgeType {
-    READ("열정적인 독자", "완독 수 {0}권 돌파!"),
-    ARCHIVE("꼼꼼한 기록자", "기록카드 수 {0}개 돌파!"),
-    ONELINE("성실한 평가자", "한줄평 수 {0}개 돌파!"),
-    LEVEL("레벨업 마스터", "레벨 {0} 달성!"),
+    READ("열정적인 독자", "완독 수 {0}권 돌파!", "책 {0}권 읽기"),
+    ARCHIVE("꼼꼼한 기록자", "기록 수 {0}개 돌파!", "기록 {0}개 작성하기"),
+    ONELINE("성실한 평가자", "한줄평 수 {0}개 돌파!", "한줄평 {0}개 작성하기"),
+    LEVEL("레벨업 마스터", "레벨 {0} 달성!", "레벨 {0}"),
     ;
 
     private final String title;
     private final String description;
+    private final String modalMessage;
 }
