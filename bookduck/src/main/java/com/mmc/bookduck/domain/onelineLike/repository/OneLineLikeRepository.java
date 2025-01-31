20524a4b1b073dae505cfc8403b3b6490233a282
hyejin2234 <hhj020304@naver.com>
2025-01-31 21:58:37 +0900

    fix: 서재에서 책 삭제 시 아카이브(리뷰,발췌), 한줄평 삭제 & 폴더에서 삭제

--- bookduck/src/main/java/com/mmc/bookduck/domain/onelineLike/repository/OneLineLikeRepository.java
+++ bookduck/src/main/java/com/mmc/bookduck/domain/onelineLike/repository/OneLineLikeRepository.java
@@ -3,11 +3,14 @@ package com.mmc.bookduck.domain.onelineLike.repository;
 import com.mmc.bookduck.domain.oneline.entity.OneLine;
 import com.mmc.bookduck.domain.onelineLike.entity.OneLineLike;
 import com.mmc.bookduck.domain.user.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface OneLineLikeRepository extends JpaRepository<OneLineLike, Long> {
     Optional<OneLineLike> findByOneLineAndUser(OneLine oneLine, User user); // OneLine에 User의 좋아요가 있는 지
+
+    List<OneLineLike> findAllByOneLine(OneLine line);
 }
 
